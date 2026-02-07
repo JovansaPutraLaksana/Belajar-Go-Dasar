f79afd07582ce8c23dbf9247db0f723383edec21
Jovansa Putra Laksana <jovansaputralaksana@gmail.com>
2026-02-07 16:29:34 +0700

    package

--- helper/34-helper.go
+++ helper/34-helper.go
@@ -1,5 +1,12 @@
 package helper
 
+var version = "1.0.0"
+var AppName = "Belajar Go"
+
+func getVersion() string {
+	return version
+}
+
 func SayHello(name string) string {
 	return "Hello, " + name + "!"
 }
